5ef23158dcb2c05cdc8f709fc0cc2a9fbb1c52a9
hamihastam <120744411+hamihastam@users.noreply.github.com>
2024-02-16 11:51:34 +0330

    _worker.js

--- _worker.js
+++ _worker.js
@@ -3,7 +3,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = '6c6bc3ea-f60d-4bfc-88d3-774a2ba19fae';
 
 const proxyIPs = ['23.162.136.169', 'cdn.xn--b6gac.eu.org', 'cdn-all.xn--b6gac.eu.org', 'edgetunnel.anycast.eu.org'];
 
